5a545aec2f14f4229eb09748a286bef4cf1e7221
Laura Kolker <laura@appfusions.com>
2010-09-11 14:05:36 +0000

    Universal Wiki Converter - uwc-12 - Way to upload a directory of documents to a confluence. Each directory in the tree gets a page. Each file in the tree is attached to its directory's page. To use, point pages setting to a dummy page. (All the magic happens in the hierarchy, but we have to tell the UWC that we're working on something) -- use conf/converter.docdirectory.properties -- Config properties include: --- doc-directory-attachments: (required) identify attachments directory --- doc-directory-root: (optional) assign the root page. default is Home --- doc-directory-template: (optional) tell the UWC what content to give each page. Default is attachments macro --- doc-directory-exclude: (optional) use a regex to exclude files in the directory. files matching regex will be excluded from being attached. directories that match regex will be excluded from being examined.
    git-svn-id: https://studio.plugins.atlassian.com/svn/UWC/devel@154531 2c54a935-e501-0410-bc05-97a93f6bca70

---
+++ src/com/atlassian/uwc/hierarchies/DocDirectoryHierarchy.java
@@ -0,0 +1,155 @@
+package com.atlassian.uwc.hierarchies;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import sun.rmi.runtime.GetThreadPoolAction;
+
+import com.atlassian.uwc.ui.FileUtils;
+import com.atlassian.uwc.ui.Page;
+
+public class DocDirectoryHierarchy extends FilepathHierarchy {
+
+
+	private static final String PROP_ATT = "doc-directory-attachments";
+	private static final String PROP_ROOT = "doc-directory-root";
+	private static final String PROP_TEMPLATE = "doc-directory-template";
+	private static final String PROP_EXCLUDE = "doc-directory-exclude";
+	
+	private static final String DEFAULT_ROOT = "Home";
+	private static final String DEFAULT_TEMPLATE = "{attachments}\n";
+
+	private File attdir;
+	private String template;
+	private FileFilter filter;
+	
+	private File getAttachmentsDirectory() {
+		if (attdir == null) {
+			String attdirpath = getProperties().getProperty(PROP_ATT, "");
+			attdir = new File(attdirpath);
+			if (!attdir.exists()) {
+				throw new IllegalArgumentException(PROP_ATT + " does not exist.");
+			}
+			if (!attdir.isDirectory()) {
+				throw new IllegalArgumentException(PROP_ATT + " is not a directory.");
+			}
+			if (attdir.list().length < 1) {
+				throw new IllegalArgumentException(PROP_ATT + " is empty.");
+			}
+		}
+		return attdir;
+	}
+	
+	public HierarchyNode buildHierarchy(Collection<Page> pages) {
+		File attachmentsDirectory = getAttachmentsDirectory(); 
+		HierarchyNode root = getRootNode();
+		log.info("Building Hierarchy.");
+
+		HierarchyNode pen = getPenultimateNode(getRootName(), root);
+		
+		FileFilter filter = getExcludeFilter();
+		File[] files = (filter == null)?attachmentsDirectory.listFiles():
+										attachmentsDirectory.listFiles(filter);
+		
+		log.debug("foreach in attachmentsdirectory: " + attachmentsDirectory.getAbsolutePath());
+		for (File child: files) {
+			log.debug(".. file: " + child.getPath());
+			buildRelationships(child, pen);
+		}
+		
+		return root;
+	}
+	
+	private HierarchyNode getPenultimateNode(String rootname, HierarchyNode root) {
+		HierarchyNode pen;
+		if (rootname != null && !"".equals(rootname)) {
+			log.debug("Page root set to: " + rootname);
+			pen = new HierarchyNode();
+			pen.setName(rootname);
+			root.addChild(pen);
+		}
+		else pen = root;
+		return pen;
+	}
+	
+	protected String getRootName() {//provided by the misc props framework
+		return getProperties().getProperty(PROP_ROOT, DEFAULT_ROOT);
+	}
+	
+	private FileFilter getExcludeFilter() {
+		if (this.filter == null) {
+			final String regex = getProperties().getProperty(PROP_EXCLUDE, null);
+			if (regex == null) return null;
+			this.filter = new FileFilter() {
+				Pattern p = Pattern.compile(regex);
+				public boolean accept(File file) {
+					String name = file.getName();
+					Matcher m = p.matcher(name);
+					return !m.matches();
+				}
+			};
+		}
+		return this.filter;
+	}
+
+	protected void buildRelationships(File file, HierarchyNode root) {
+		if (file.isDirectory()) { 
+			//create Page and Node
+			String name = file.getName();
+			Page page = new Page(null);
+            page.setName(name);
+            page.setOriginalText(getTemplate());
+            page.setConvertedText(getTemplate());
+            page.setPath(name); //needed by auto-detect spacekeys feature
+            HierarchyNode node = new HierarchyNode(page, root);
+            
+            //recurse
+            FileFilter filter = getExcludeFilter();
+    		File[] next = (filter == null)?file.listFiles():
+    									   file.listFiles(filter);
+    		for (File child : next) {
+				buildRelationships(child, node);
+			}
+		}
+		else { //assign as attachment
+			Page page = root.getPage();
+			if (page == null) {
+				page = new Page(null);
+				page.setName(root.getName());
+				page.setPath(root.getName());
+				page.setOriginalText(getTemplate());
+				page.setConvertedText(getTemplate());
+				root.setPage(page);
+			}
+			page.addAttachment(file);
+		}
+	}
+
+	private String getTemplate() {
+		if (template == null) {
+			String templatepath = getProperties().getProperty(PROP_TEMPLATE, "");
+			File templatefile = new File(templatepath);
+			if (!templatefile.exists()) {
+				log.error("doc-directory-template file does not exist: " + templatepath + "\nUsing default.");
+				template = DEFAULT_TEMPLATE;
+			}
+			if (!templatefile.isFile()) {
+				log.error("doc-directory-template file is not a file: " + templatepath + "\nUsing default.");
+				template = DEFAULT_TEMPLATE;
+			}
+			try {
+				template = FileUtils.readTextFile(templatefile);
+			} catch (IOException e) {
+				log.error("Could not read doc-directory-template file: " + templatepath + "\nUsing default.");
+				e.printStackTrace();
+				template = DEFAULT_TEMPLATE;
+			}
+		}
+		return template;
+	}
+	
+}
---
+++ src/com/atlassian/uwc/hierarchies/DocDirectoryHierarchyTest.java
@@ -0,0 +1,202 @@
+package com.atlassian.uwc.hierarchies;
+
+import java.io.File;
+import java.util.Properties;
+import java.util.Set;
+import java.util.Vector;
+
+import junit.framework.TestCase;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+import com.atlassian.uwc.filters.NoSvnFilter;
+import com.atlassian.uwc.ui.Page;
+
+public class DocDirectoryHierarchyTest extends TestCase {
+
+	DocDirectoryHierarchy tester = null;
+	Logger log = Logger.getLogger(this.getClass());
+	protected void setUp() throws Exception {
+		tester = new DocDirectoryHierarchy();
+		PropertyConfigurator.configure("log4j.properties");
+		Properties props = new Properties();
+		props.setProperty("doc-directory-attachments", "sampleData/docdirectory/attachments");
+		tester.setProperties(props);
+	}
+
+	public void testBuildHierarchy() {
+		Vector<Page> pages = new Vector<Page>();
+		File sampledir = new File("sampleData/docdirectory/testtemplate.txt");
+		assertTrue(sampledir.exists());
+		
+		Page page = new Page(sampledir);
+		page.setOriginalText("");
+		page.setConvertedText(page.getOriginalText());
+		page.setUnchangedSource(page.getOriginalText());
+		page.setName(sampledir.getName().replaceFirst("\\.txt$", ""));
+		pages.add(page);
+		
+		HierarchyNode actual = tester.buildHierarchy(pages);
+		assertNotNull(actual); //root node
+		assertNull(actual.getName());
+		assertNull(actual.getPage());
+		assertNull(actual.getParent());
+		assertNotNull(actual.getChildren());
+		
+		Set<HierarchyNode> children1 = actual.getChildren();
+		assertEquals(1, children1.size());
+		Vector<HierarchyNode> nodes1 = new Vector<HierarchyNode>();
+		nodes1.addAll(children1);
+		assertTrue((nodes1.get(0)).getName().equals("Home"));
+		assertTrue(nodes1.get(0).getPage().getAttachments().size() == 1);
+		Object object = nodes1.get(0).getPage().getAttachments().toArray()[0];
+		assertTrue(object instanceof File);
+		File file = (File) object;
+		assertTrue("test.txt".equals(file.getName()));
+		
+		Set<HierarchyNode> children2 = nodes1.get(0).getChildren();
+		assertEquals(3, children2.size());
+		Vector<HierarchyNode> nodes2 = new Vector<HierarchyNode>();
+		nodes2.addAll(children2);
+		for (HierarchyNode node : nodes2) {
+			assertTrue(node.getName().equals("a") ||
+					node.getName().equals("b") ||
+					node.getName().equals("c")
+					);
+			if (node.getName().equals("a")) {
+				assertTrue(node.getPage().getAttachments().size() == 2);
+				assertTrue(node.getPage().getAttachments().toArray()[0] instanceof File);
+				File filea = (File) node.getPage().getAttachments().toArray()[0];
+				assertTrue("test1.txt".equals(filea.getName())||
+						"test2.txt".equals(filea.getName())
+						);
+				Set<HierarchyNode> children3 = node.getChildren();
+				assertEquals(1, children3.size());
+				Vector<HierarchyNode> nodes3 = new Vector<HierarchyNode>();
+				nodes3.addAll(children3);
+				assertTrue((nodes3.get(0)).getName().equals("d"));
+				assertTrue(nodes3.get(0).getPage().getAttachments().size() == 1);
+				assertTrue(nodes3.get(0).getPage().getAttachments().toArray()[0] instanceof File);
+				File filed = (File) nodes3.get(0).getPage().getAttachments().toArray()[0];;
+				assertTrue("test3.txt".equals(filed.getName()));
+			}
+			if (node.getName().equals("b")) {
+				assertTrue(node.getPage().getAttachments().size() == 1);
+				assertTrue(node.getPage().getAttachments().toArray()[0] instanceof File);
+				File fileb = (File) node.getPage().getAttachments().toArray()[0];
+				assertTrue("test4.txt".equals(fileb.getName()));
+			}
+			if (node.getName().equals("c")) {
+				assertTrue(node.getPage().getAttachments().size() == 1);
+				assertTrue(node.getPage().getAttachments().toArray()[0] instanceof File);
+				File filec = (File) node.getPage().getAttachments().toArray()[0];
+				assertTrue("test5.txt".equals(filec.getName()));
+			}
+		}
+	}
+	
+	public void testBuildHierarchy_root() {
+		Properties props = tester.getProperties();
+		props.setProperty("doc-directory-root", "Test");
+		tester.setProperties(props);
+		Vector<Page> pages = new Vector<Page>();
+		File sampledir = new File("sampleData/docdirectory/testtemplate.txt");
+		assertTrue(sampledir.exists());
+		
+		Page page = new Page(sampledir);
+		page.setOriginalText("");
+		page.setConvertedText(page.getOriginalText());
+		page.setUnchangedSource(page.getOriginalText());
+		page.setName(sampledir.getName().replaceFirst("\\.txt$", ""));
+		pages.add(page);
+		
+		HierarchyNode actual = tester.buildHierarchy(pages);
+		assertNotNull(actual); //root node
+		assertNull(actual.getName());
+		assertNull(actual.getPage());
+		assertNull(actual.getParent());
+		assertNotNull(actual.getChildren());
+		
+		Set<HierarchyNode> children1 = actual.getChildren();
+		assertEquals(1, children1.size());
+		Vector<HierarchyNode> nodes1 = new Vector<HierarchyNode>();
+		nodes1.addAll(children1);
+		assertTrue((nodes1.get(0)).getName().equals("Test"));
+	}
+	
+	public void testBuildHierarchy_template() {
+		Properties props = tester.getProperties();
+		props.setProperty("doc-directory-template", "/Users/laura/Code/Subversion/uwc-spac/devel/sampleData/docdirectory/testtemplate.txt");
+		tester.setProperties(props);
+		Vector<Page> pages = new Vector<Page>();
+		File sampledir = new File("sampleData/docdirectory/testtemplate.txt");
+		assertTrue(sampledir.exists());
+		
+		Page page = new Page(sampledir);
+		page.setOriginalText("");
+		page.setConvertedText(page.getOriginalText());
+		page.setUnchangedSource(page.getOriginalText());
+		page.setName(sampledir.getName().replaceFirst("\\.txt$", ""));
+		pages.add(page);
+		
+		HierarchyNode actual = tester.buildHierarchy(pages);
+		assertNotNull(actual); //root node
+		assertNull(actual.getName());
+		assertNull(actual.getPage());
+		assertNull(actual.getParent());
+		assertNotNull(actual.getChildren());
+		
+		Set<HierarchyNode> children1 = actual.getChildren();
+		assertEquals(1, children1.size());
+		Vector<HierarchyNode> nodes1 = new Vector<HierarchyNode>();
+		nodes1.addAll(children1);
+		HierarchyNode node = nodes1.get(0);
+		assertTrue((node).getName().equals("Home"));
+		assertEquals("Testing\n{attachments}\n", node.getPage().getConvertedText());
+	}
+	
+	public void testBuildHierarhcy_exclude() {
+		Properties props = tester.getProperties();
+		props.setProperty("doc-directory-exclude", "c");
+		tester.setProperties(props);
+		Vector<Page> pages = new Vector<Page>();
+		File sampledir = new File("sampleData/docdirectory/testtemplate.txt");
+		assertTrue(sampledir.exists());
+		
+		Page page = new Page(sampledir);
+		page.setOriginalText("");
+		page.setConvertedText(page.getOriginalText());
+		page.setUnchangedSource(page.getOriginalText());
+		page.setName(sampledir.getName().replaceFirst("\\.txt$", ""));
+		pages.add(page);
+		
+		HierarchyNode actual = tester.buildHierarchy(pages);
+		assertNotNull(actual); //root node
+		assertNull(actual.getName());
+		assertNull(actual.getPage());
+		assertNull(actual.getParent());
+		assertNotNull(actual.getChildren());
+		
+		Set<HierarchyNode> children1 = actual.getChildren();
+		assertEquals(1, children1.size());
+		Vector<HierarchyNode> nodes1 = new Vector<HierarchyNode>();
+		nodes1.addAll(children1);
+		assertTrue((nodes1.get(0)).getName().equals("Home"));
+		assertTrue(nodes1.get(0).getPage().getAttachments().size() == 1);
+		Object object = nodes1.get(0).getPage().getAttachments().toArray()[0];
+		assertTrue(object instanceof File);
+		File file = (File) object;
+		assertTrue("test.txt".equals(file.getName()));
+		
+		Set<HierarchyNode> children2 = nodes1.get(0).getChildren();
+		assertEquals(2, children2.size());
+		Vector<HierarchyNode> nodes2 = new Vector<HierarchyNode>();
+		nodes2.addAll(children2);
+		for (HierarchyNode node : nodes2) {
+			assertTrue(node.getName(), node.getName().equals("a") ||
+					node.getName().equals("b"));
+		}
+	}
+	
+}
