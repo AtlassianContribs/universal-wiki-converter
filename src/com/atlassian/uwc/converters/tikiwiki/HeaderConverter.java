37298b07a74ac4d36b13887bedbd87b54d917678
Laura Kolker <laura@appfusions.com>
2011-04-12 16:10:22 +0000

    Universal Wiki Converter -- UWC-494 header handling changes
    git-svn-id: https://studio.plugins.atlassian.com/svn/UWC/devel@159690 2c54a935-e501-0410-bc05-97a93f6bca70

--- src/com/atlassian/uwc/converters/tikiwiki/HeaderConverter.java
+++ src/com/atlassian/uwc/converters/tikiwiki/HeaderConverter.java
@@ -10,35 +10,35 @@ import com.atlassian.uwc.ui.Page;
 
 public class HeaderConverter extends BaseConverter {
 
-	Logger log = Logger.getLogger(this.getClass());
-	public void convert(Page page) {
-		log.debug("Converting Headers - starting");
-		String input = page.getOriginalText();
-		String converted = convertHeaders(input);
-		page.setConvertedText(converted);
-		log.debug("Converting Header - complete");
-	}
-	
-	Pattern headerPattern = Pattern.compile("(?<=^|\n)(!+)(.*)");
-	protected String convertHeaders(String input) {
-		Matcher headerFinder = headerPattern.matcher(input);
-		
-		StringBuffer sb = new StringBuffer();
-		boolean found = false;
-		while (headerFinder.find()) {
-			found = true;
-			String marks = headerFinder.group(1);
-			String contents = headerFinder.group(2);
-			int level = marks.length();
-			if (!contents.startsWith(" ")) contents = " " + contents;
-			String replacement = "h" + level + "." + contents;
-			headerFinder.appendReplacement(sb, replacement);
-		}
-		if (found) {
-			headerFinder.appendTail(sb);
-			return sb.toString();
-		}
-		return input; 
-	}
+    Logger log = Logger.getLogger(this.getClass());
+    public void convert(Page page) {
+        log.debug("Converting Headers - starting");
+        String input = page.getOriginalText();
+        String converted = convertHeaders(input);
+        page.setConvertedText(converted);
+        log.debug("Converting Header - complete");
+    }
+
+    Pattern headerPattern = Pattern.compile("(?<=^|\n)(!+)(.*)");
+    protected String convertHeaders(String input) {
+        Matcher headerFinder = headerPattern.matcher(input);
+
+        StringBuffer sb = new StringBuffer();
+        boolean found = false;
+        while (headerFinder.find()) {
+            found = true;
+            String marks = headerFinder.group(1);
+            String contents = headerFinder.group(2);
+            int level = marks.length();
+            if (!contents.startsWith(" ")) contents = " " + contents;
+            String replacement = "h" + level + "." + contents;
+            headerFinder.appendReplacement(sb, Matcher.quoteReplacement(replacement));
+        }
+        if (found) {
+            headerFinder.appendTail(sb);
+            return sb.toString();
+        }
+        return input;
+    }
 
 }
